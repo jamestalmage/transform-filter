1af5d4c37d0f1b840b0ce65e780b57a6e1f195c1
James Talmage <james.talmage@jrtechnical.com>
2015-02-24 02:48:11 -0500

    consistent whitespace

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ var path = require('path') ;
 module.exports = transformFilter;
 
 function transformFilter (includePattern, excludePattern, transform){
-  if(typeof excludePattern === 'function'){
+  if (typeof excludePattern === 'function') {
     transform = excludePattern;
     excludePattern = null;
   }
 
-  function include(path){
+  function include (path) {
     return includePattern ? !!multimatch(path, includePattern).length : true;
   }
 
-  function exclude(path){
+  function exclude (path) {
     return excludePattern ? !!multimatch(path, excludePattern).length : false;
   }
 
-  function test(file){
+  function test (file) {
     var p = path.relative(process.cwd(), file);
     return !exclude(p) && include(p);
   }
 
-  return function(file, opts){
+  return function (file, opts) {
     var pass = test(file);
     return pass ? transform.call(this,file, opts) : through();
   };
